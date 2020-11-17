013adf223bc5895dc01308ebcebe5179cc633a54
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-11-18 00:40:00 +0100

    bonus
    -keydown left and right
    -bullet change slider
    -class active responsive

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -5,7 +5,7 @@
 var app = new Vue({
   el: "#app",
   data: {
-    pic: [
+    pics: [
       "img/image1.jpg",
       "img/image2.jpg",
       "img/image3.jpg",
@@ -18,11 +18,11 @@ var app = new Vue({
       // attivando la funzione(con @click in html sulla freccia avanti) l'indice assegnato alle immagini (che di default è 0) aumenta di uno, cambiando l'immagine in quella successiva
       this.indexPic = this.indexPic + 1;
       //se però questo indice supera la lunghezza dell'arrey pic, ovvero prova a passare all'immagine 5 in questo array da quattro
-      if(this.indexPic > (this.pic.length - 1)){
+      if(this.indexPic > (this.pics.length - 1)){
         //allora si torna all'immagine di partenza, con indice0
         this.indexPic = 0;
       }
-      console.log('right', this.indexPic);
+      console.log('next', this.indexPic);
     },
 
     prevPic: function () {
@@ -31,10 +31,15 @@ var app = new Vue({
       //se però questo indice vuole trasformare l'indice minore presente nell'array, che è 0
       if (this.indexPic < 0){
         //in tal caso l'indice viene eguagliato alla lunghezza dell'array -1 ( dato che l'array è lungo 4 ma gli indici partono da 0) prendendo l'ultima immagine dell'array
-        this.indexPic = this.pic.length - 1;
+        this.indexPic = this.pics.length - 1;
       }
-      console.log('left', this.indexPic);
-    }
+      console.log('prev', this.indexPic);
+    },
+
+    cambiaImmagine: function (indexPallino) {
+    this.indexPic = indexPallino;
+  }
+
   },
   // mounted mi permette di inserire laddeventlistener a vue
   mounted() {
